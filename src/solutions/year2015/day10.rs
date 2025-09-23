6846ae12ba4f33dec08b6c04c8f723cc3b882530
Callum Oakley <hello@callumoakley.net>
2025-09-23 08:48:53 +0100

    2015 10 chunk_by

--- src/solutions/year2015/day10.rs
+++ src/solutions/year2015/day10.rs
@@ -1,21 +1,10 @@
 use std::fmt::Write;
 
-// TODO it would be nice to rewrite this in terms of group_by when that hits
-// stable: https://doc.rust-lang.org/std/primitive.slice.html#method.group_by
 fn look_and_say(s: &str) -> String {
     let mut res = String::new();
-    let mut digit = s.chars().next().unwrap();
-    let mut count = 1;
-    for c in s.chars().skip(1) {
-        if c == digit {
-            count += 1;
-        } else {
-            write!(res, "{count}{digit}").unwrap();
-            digit = c;
-            count = 1;
-        }
+    for chunk in s.as_bytes().chunk_by(|a, b| a == b) {
+        write!(res, "{}{}", chunk.len(), char::from(chunk[0])).unwrap();
     }
-    write!(res, "{count}{digit}").unwrap();
     res
 }
 
